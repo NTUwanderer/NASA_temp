2200157e896a3f27ee649ee9ba2284a11eae128f
root <root@localhost.localdomain>
2017-04-28 21:34:15 -0400

    Update startup_cmd

--- startup_cmd.sh
+++ startup_cmd.sh
@@ -1,4 +1,4 @@
-yum install -y virt-install qemu-kvm libvirt
+yum install -y virt-install qemu-kvm libvirt vim
 systemctl start libvirtd
 systemctl enable libvirtd
 curl http://ftp.tc.edu.tw/Linux/CentOS/7/isos/x86_64/CentOS-7-x86_64-Minimal-1611.iso -o CentOS-7.iso
