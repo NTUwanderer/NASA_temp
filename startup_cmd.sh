d29d009f5e38200f043505d20f6d7faceeb17495
NTUwanderer <harveyycyang@gmail.com>
2017-04-29 09:21:19 +0800

    Add startup_cmd & helpful_notes

---
+++ startup_cmd.sh
@@ -0,0 +1,5 @@
+yum install -y virt-install qemu-kvm libvirt
+systemctl start libvirtd
+systemctl enable libvirtd
+curl http://ftp.tc.edu.tw/Linux/CentOS/7/isos/x86_64/CentOS-7-x86_64-Minimal-1611.iso -o CentOS-7.iso
+mv CentOS-7.iso /var/lib/libvirtd/images/
