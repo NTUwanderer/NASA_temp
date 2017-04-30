cab276bd5fe91d2618106cdf80c0ad14c874f281
root <root@localhost.localdomain>
2017-04-29 23:29:27 -0400

    chmod

--- startup_cmd.sh
+++ startup_cmd.sh
@@ -2,4 +2,4 @@ yum install -y virt-install qemu-kvm libvirt vim
 systemctl start libvirtd
 systemctl enable libvirtd
 curl http://ftp.tc.edu.tw/Linux/CentOS/7/isos/x86_64/CentOS-7-x86_64-Minimal-1611.iso -o CentOS-7.iso
-mv CentOS-7.iso /var/lib/libvirtd/images/
+mv CentOS-7.iso /var/lib/libvirt/images/
